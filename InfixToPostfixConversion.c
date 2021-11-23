36d023327e1b35039b31ca35eb9c3ec5300a90bd
Aditya <69726226+ADITYAbasude@users.noreply.github.com>
2021-11-23 12:44:23 +0530

    .

--- InfixToPostfixConversion.c
+++ InfixToPostfixConversion.c
@@ -1,17 +1,26 @@
+// -----------------header files declaration-----------------
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+
+// --------------stack declaration using struct-----------
 struct stack
 {
     int size;
     int top;
     int *arr;
 };
+
+// --------stacktop (function) -----------------
 int stackTop(struct stack *sp)
 {
     return sp->arr[sp->top];
 }
+
+
+
+// ----------------------check it is operator or not-----------
 int operanderCheck(char infix)
 {
     if (infix == '+' || infix == '-' || infix == '*' || infix == '/' || infix == '$')
@@ -23,6 +32,10 @@ int operanderCheck(char infix)
         return 0;
     }
 }
+
+
+
+// -----------------is full (function)-----------------------
 int isFull(struct stack *ptr)
 {
     if (ptr->top == ptr->size - 1)
@@ -34,19 +47,29 @@ int isFull(struct stack *ptr)
         return 0;
     }
 }
+
+
+
+
+
+// -----------------------insert the element into the stack (function)----------------
 void push(struct stack *ptr, char val)
 {
-    if (isFull(ptr))
+    if (isFull(ptr))    //check stack is full or not
     {
         printf("Stack Overflow! Cannot push %d to the stack\n", val);
     }
     else
     {
-        ptr->top++;
-        ptr->arr[ptr->top] = val;
+        ptr->top++;         
+        ptr->arr[ptr->top] = val;           //inserting the values 
     }
 }
 
+
+
+
+// -------------check which operator is biggest-----------------
 int operanderBigger(char infix)
 {
     if (infix == '$')
@@ -55,9 +78,15 @@ int operanderBigger(char infix)
         return 3;
     else if (infix == '-' || infix == '+')
         return 2;
+    else if (infix == '(')
+        return 0;
     else
         return 0;
 }
+
+
+
+// -------------------check empty (function)-----------------------
 int isEmpty(struct stack *ptr)
 {
     if (ptr->top == -1)
@@ -67,9 +96,11 @@ int isEmpty(struct stack *ptr)
     return 0;
 }
 
+
+// ----------------------delecting element from stack (function)-------------------
 char pop(struct stack *ptr)
 {
-    if (isEmpty(ptr))
+    if (isEmpty(ptr))           // checking stack empty or not
     {
         printf("Stack Underflow! Cannot pop from the stack\n");
         return -1;
@@ -87,24 +118,18 @@ char *convertInfixToPostfix(char *infix, int size1)
     // -------------variable declaration------------
     int i = 0, j = 0;
 
-    // ------------stake values decleration---------
+    // ------------stake dynamically decleration---------
     struct stack *s = (struct stack *)malloc(sizeof(struct stack));
     s->size = size1;
     s->top = -1;
     s->arr = (int *)malloc(s->size * sizeof(int));
 
-    // -------------coding of infix to postfix-------------
+    // -------------Logic of infix to postfix expresion-------------
     char *postfix = (char *)malloc((strlen(infix) + 1) * sizeof(char));
 
     while (infix[i] != '\0')
     {
-
-        if (infix[i] == '(')
-        {
-            push(s, infix[i]);
-            i++;
-        }
-        else if (infix[i] == ')')
+        if (operanderBigger(infix[i]) > operanderBigger(stackTop(s)))
         {
             push(s, infix[i]);
             i++;
@@ -112,13 +137,18 @@ char *convertInfixToPostfix(char *infix, int size1)
 
         else if (infix[i] == '(')
         {
-            while (infix[i] != ')')
+            push(s, infix[i]);
+            i++;
+        }
+        else if (infix[i] == ')')
+        {
+            while (s->arr[s->top] != '(')
             {
                 postfix[j] = pop(s);
                 j++;
-                s->top++;
             }
             i++;
+            pop(s);
         }
 
         else
@@ -127,11 +157,6 @@ char *convertInfixToPostfix(char *infix, int size1)
             {
                 postfix[j] = infix[i];
                 j++;
-                i++;
-            }
-            else if (operanderBigger(infix[i]) > operanderBigger(stackTop(s)))
-            {
-                push(s, infix[i]);
                 i++;
             }
 
@@ -143,31 +168,24 @@ char *convertInfixToPostfix(char *infix, int size1)
         }
     }
 
+
+// ---------------removing all remaining element from stack-----------------
     while (!isEmpty(s))
     {
-        if (s->arr[s->top] == '(')
-        {
-            pop(s);
-        }
-        else if (s->arr[s->top] == ')')
-        {
-            pop(s);
-        }
-
-        else
-        {
-            postfix[j] = pop(s);
-            j++;
-        }
+        postfix[j] = pop(s);
+        j++;
     }
     postfix[j] = '\0';
-    return postfix;
+    return postfix;         //returning postfix expersion
 }
+
+
+// -------------int main program -----------------
 
 int main()
 {
-    char *exp = "A+B/C*(D-AF)H";
-    int size = sizeof(exp) / sizeof(exp[0]);
-    printf("%s", convertInfixToPostfix(exp, size));
+    char *exp = "A+B/d*6C-(D+/D-A)FH+*";   //Infix expresion 
+    int size = sizeof(exp) / sizeof(exp[0]);    //finding size of exp
+    printf("%s", convertInfixToPostfix(exp, size));     //it will print the postfix expresion
     return 0;
 }
--- tempCodeRunnerFile.c
+++
@@ -1,2 +0,0 @@
-
-        printf("Queue is overflow 
