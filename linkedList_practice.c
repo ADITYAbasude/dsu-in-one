ecead64f5a0b19352617572086e20a17ca518079
ADITYAbasude <adityabasude13@gmail.com>
2023-10-10 15:09:36 +0530

    updated code

--- linkedList_practice.c
+++ linkedList_practice.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 struct list
 {
     int data;
     struct list *next;
 };
 
-struct list * insertAtFirst(struct list *head , int data){
+struct list *insertAtFirst(struct list *head, int data)
+{
     struct list *node = (struct list *)malloc(sizeof(struct list));
     node->data = data;
     node->next = head;
     return node;
 }
 
-struct list * insertAtBetween(struct list * head , int data , int index){
+struct list *insertAtBetween(struct list *head, int data, int index)
+{
+    // create a new node
     struct list *node = (struct list *)malloc(sizeof(struct list));
+    // take copy of head
     struct list *p = head;
+    // traverse to the index - 1 node
     int i = 0;
-    while (i!=index-1)
+    while (i != index - 1)
     {
         p = p->next;
         i++;
@@ -26,13 +31,15 @@ struct list * insertAtBetween(struct list * head , int data , int index){
     node->next = p->next;
     p->next = node;
     return head;
-    
 }
 
-
-struct list * insertionAtEnd(struct list *head , int data){
+struct list *insertionAtEnd(struct list *head, int data)
+{
+    // create a new node
     struct list *node = (struct list *)malloc(sizeof(struct list));
+    // take copy of head
     struct list *p = head;
+    // traverse to the last node
     while (p->next != NULL)
     {
         p = p->next;
@@ -43,18 +50,20 @@ struct list * insertionAtEnd(struct list *head , int data){
     return head;
 }
 
-struct list * deletionAtFirst(struct list *head){
+struct list *deletionAtFirst(struct list *head)
+{
     struct list *ptr = head;
     head = head->next;
     free(ptr);
     return head;
 }
 
-struct list * deletionAtBetween(struct list *head , int index){
+struct list *deletionAtBetween(struct list *head, int index)
+{
     struct list *ptr = head;
-    struct list *q  = head->next;
+    struct list *q = head->next;
     int i = 0;
-    while (i!= index-1)
+    while (i != index - 1)
     {
         ptr = ptr->next;
         q = q->next;
@@ -63,15 +72,14 @@ struct list * deletionAtBetween(struct list *head , int index){
     ptr->next = q->next;
     free(q);
     return head;
-    
 }
 
-
-struct list * deletionAtEnd(struct list *head){
+struct list *deletionAtEnd(struct list *head)
+{
     struct list *ptr = head;
-    struct list *q  = head->next;
+    struct list *q = head->next;
     int i = 0;
-    while (q->next!= NULL )
+    while (q->next != NULL)
     {
         ptr = ptr->next;
         q = q->next;
@@ -80,17 +88,16 @@ struct list * deletionAtEnd(struct list *head){
     ptr->next = NULL;
     free(q);
     return head;
-    
 }
 
-void display(struct list *head){
+void display(struct list *head)
+{
     while (head != NULL)
     {
-        printf("%d\t"  ,  head->data);
+        printf("%d\t", head->data);
         head = head->next;
     }
     printf("\n");
-    
 }
 
 int main()
@@ -101,14 +108,14 @@ int main()
     head = (struct list *)malloc(sizeof(struct list));
     head->data = 10;
     head->next = NULL;
-    head = insertAtFirst(head , 5);
-    head = insertAtFirst(head , 6);
+    head = insertAtFirst(head, 5);
+    head = insertAtFirst(head, 6);
 
     // display(head);
-    head = insertAtBetween(head , 11 , 2);
-    // display(head);
-    head = insertionAtEnd(head , 100);
+    head = insertAtBetween(head, 11, 2);
     display(head);
+    // head = insertionAtEnd(head, 100);
+    // display(head);
 
     // head = deletionAtFirst(head);
     // display(head);
